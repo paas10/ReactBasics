8f7190f1997baae44dd9c85dad2989f3179de3ac
Pablo <pablo.andre_alvarado@hotmail.com>
2022-03-11 12:06:10 -0600

    Props por defecto

--- src/components/Saludar.js
+++ src/components/Saludar.js
@@ -2,11 +2,12 @@ import React from "react";
 
 export default function Saludar(props) {
   const { userInfo, saludarFn } = props;
+  const { name = "Anónimo" } = userInfo
   console.log(userInfo)
 
   return (
     <div>
-      <button onClick={() => saludarFn(userInfo.name)}>Saludar</button>
+      <button onClick={() => saludarFn(name)}>Saludar</button>
     </div>
   )
 }
