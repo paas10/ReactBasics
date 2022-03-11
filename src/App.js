d55a23540f73d10bfd6a742a20bd3c23acb39c28
Pablo <pablo.andre_alvarado@hotmail.com>
2022-03-11 11:35:14 -0600

    Pass properties and functions through Component props between child and father

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 import HolaMundo from './components/HolaMundo';
 import AdiosMundo from './components/AdiosMundo';
+import Saludar from './components/Saludar';
 
 function App() {
+
+  const user = { 
+    name: 'Pablo Alvarado',
+    age: 23,
+    color: 'Azul'
+  }
+
+  const saludarFn = (name) => {
+    console.log('Hola ', name)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <HolaMundo/>
-        <AdiosMundo/>
+        {/* <HolaMundo/>
+        <AdiosMundo/> */}
+
+        <Saludar userInfo={user} saludarFn={saludarFn}/>
 
       </header>
     </div>
---
+++ src/components/Saludar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Saludar(props) {
+  console.log(props)
+
+  return (
+    <div>
+      <button onClick={() => props.saludarFn(props.userInfo.name)}>Saludar</button>
+    </div>
+  )
+}
