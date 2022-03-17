ef3e23af2ea00fb4ead5ee42cda0d941e8c22bd8
Pablo <pablo.andre_alvarado@hotmail.com>
2022-03-16 20:53:51 -0600

    Hook de estado

--- src/components/Saludar.js
+++ src/components/Saludar.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Saludar(props) {
   const { userInfo, saludarFn } = props;
   const { name = "Anónimo", age } = userInfo
   console.log(userInfo)
 
+  // Hook de estado <<useState>>
+  const [stateCar, setStateCar] = useState(false);
+
   return (
     <div>
       <button onClick={() => saludarFn(name, age)}>Saludar</button>
+
+      <hr></hr><br></br>
+
+      <h2>El coche está: {stateCar ? 'Encendido' : 'Apagado'}</h2>
+      <button onClick={() => {setStateCar(prevValue => !prevValue)}}>
+        {stateCar ? 'Apagar' : 'Encender'}
+      </button>
+
     </div>
   )
 }
